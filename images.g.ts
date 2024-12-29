640835681bcbbfd9415e19aac3a772037372b633
Martin Benda <martin.benda@omsquare.com>
2024-12-30 00:23:11 +0100

    snake game

---
+++ images.g.ts
@@ -0,0 +1,58 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "snake_body":return img`
+. . . . . . . . 
+. 6 6 6 6 6 6 . 
+. 6 2 6 6 6 6 . 
+. 6 6 6 6 2 6 . 
+. 6 6 6 6 6 6 . 
+. 6 6 2 6 6 6 . 
+. 6 6 6 6 6 6 . 
+. . . . . . . . 
+`;
+            case "image1":
+            case "snake_head":return img`
+. . . . . . . . 
+. 9 9 9 9 9 9 . 
+. 9 9 9 9 9 9 . 
+. 9 7 9 9 7 9 . 
+. 9 9 9 9 9 9 . 
+. 9 7 7 7 7 9 . 
+. 9 9 9 9 9 9 . 
+. . . . . . . . 
+`;
+            case "image3":
+            case "apple":return img`
+. . 7 7 e e . . 
+. 2 2 7 e 2 2 . 
+. 2 2 2 e 2 2 . 
+2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 
+. 2 2 2 2 2 2 . 
+. . 2 2 2 2 . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,1 +1,121 @@
- game.gameOver(false)
+interface Position { x: number, y: number }
+
+let headSprite: Sprite
+let headPosition: Position
+let direction: String
+
+let bodySprites: Sprite[]
+let bodyLength: number
+
+let appleSprite: Sprite
+
+let speed = 200
+let lastMove = game.runtime()
+
+const step = 8
+
+function start() {
+    headPosition = {x: 80, y: 64}
+    bodyLength = 3
+    bodySprites = []
+    direction = "right"
+
+    headSprite = sprites.create(assets.image`snake_head`, SpriteKind.Player)
+    headSprite.z = 100
+    headSprite.setPosition(headPosition.x, headPosition.y)
+
+    appleSprite = sprites.create(assets.image`apple`, SpriteKind.Food)
+    appleSprite.setPosition(48, 16)
+
+    // game.onUpdateInterval(250, move)
+    game.onUpdate(update)
+
+    controller.left.onEvent(ControllerButtonEvent.Pressed, function() {
+        direction = "left"
+    })
+    controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+        direction = "right"
+    })
+    controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+        direction = "up"
+    })
+    controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+        direction = "down"
+    })
+
+    controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+        bodyLength++
+    })
+
+    controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+        speed -= 10
+    })
+
+    sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite: Sprite, otherSprite: Sprite) {
+        game.over(false)
+    })
+    sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite: Sprite, otherSprite: Sprite) {
+        eat()
+    })
+}
+
+function update() {
+    if (game.runtime() - speed >= lastMove) {
+        lastMove = game.runtime() + speed
+        move()
+    }
+}
+
+function move() {
+    let bodySprite = sprites.create(assets.image`snake_body`, SpriteKind.Enemy)
+    bodySprite.setPosition(headPosition.x, headPosition.y)
+
+    bodySprites.push(bodySprite)
+    if (bodySprites.length > bodyLength) {
+        let lastBodySprite = bodySprites.shift()
+        lastBodySprite.destroy()
+    }
+
+    // 19 x 14
+
+    if (direction == "right") {
+        headPosition.x += 8
+        if (headPosition.x > 160) {
+            headPosition.x -= 160
+        }
+    } else if (direction == "left") {
+        headPosition.x -= 8
+        if (headPosition.x < 0) {
+            headPosition.x += 160
+        }
+    } else if (direction == "down") {
+        headPosition.y += 8
+        if (headPosition.y > 128) {
+            headPosition.y -= 128
+        }
+    } else if (direction == "up") {
+        headPosition.y -= 8
+        if (headPosition.y < 0) {
+            headPosition.y += 128
+        }
+    }
+
+    headSprite.setPosition(headPosition.x, headPosition.y)
+}
+
+function eat() {
+    bodyLength += 1
+    let free = []
+    for (let x = 0; x < 19; x++) {
+        for (let y = 0; y < 14; y++) {
+            let p = { x: x * step, y: y * step }
+            if (headPosition != p && bodySprites.every(e => e.x != p.x && e.y != p.y)) {
+                free.push(p)
+            }
+        }
+    }
+    let applePosition = free[randint(0, free.length-1)]
+    appleSprite.setPosition(applePosition.x, applePosition.y)
+}
+
+start()
